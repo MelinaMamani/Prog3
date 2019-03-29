8a6060fc43e7d71362a32ea5dcfca3023b2a9bfd
alumno <alumno>
2019-03-29 12:12:48 -0300

    Clase 4 Lab 3

---
+++ Clase4/EjerciciosHTML/ejercicio21/ejercicio21.ts
@@ -0,0 +1,8 @@
+function Calcular():void 
+{
+    let numUno : number = parseInt((<HTMLInputElement> document.getElementById("numeroUno")).value); 
+    let numDos : number = parseInt((<HTMLInputElement> document.getElementById("numeroDos")).value);
+    //let operadores : NodeListOf(<HTMLInputElements> document.getElementsByName("")).items;
+    
+    
+}
